152e6ad54ce89925bf2fdb330d599d945802074c
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2024-09-27 12:31:32 -1000

    Create Main.java

---
+++ Shapes/Main.java
@@ -0,0 +1,20 @@
+/**
+ * This class creates and tests the objects made in other classes.
+ * This class was made for a project.
+ * @author Keola Teson :)
+ */
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		//prints the different stats from the Main_Shapes class
+		Shapes_Methods.printCircleStats();
+		Shapes_Methods.printSquareStats();
+		Shapes_Methods.printRectangleStats();
+		Shapes_Methods.printTriangleStats();
+		Shapes_Methods.printSphereStats();
+		Shapes_Methods.printCubeStats();
+	}
+
+}
