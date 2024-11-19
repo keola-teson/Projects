477b94b6fa7d0202f45b2327444db99cca79a5ce
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2024-11-18 21:09:37 -1000

    Update Game.java

--- MakeYourOwnChoice/Game.java
+++ MakeYourOwnChoice/Game.java
@@ -14,6 +14,7 @@ public class Game
 		//calls the callSetUpClass method
 		Dialogue.callSetUpClass();
 		
+		//calls the makeFirstScene method to play scene 1
 		Dialogue.makeFirstScene();
 		
 		//closes scanner
