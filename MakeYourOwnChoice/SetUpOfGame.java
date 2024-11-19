490cabd9fe07093b07eec723e42e7544819f339e
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2024-11-18 21:08:41 -1000

    Update SetUpOfGame.java

--- MakeYourOwnChoice/SetUpOfGame.java
+++ MakeYourOwnChoice/SetUpOfGame.java
@@ -26,7 +26,7 @@ public class SetUpOfGame
 	 */
 	public static String welcomePlayer()
 	{
-		return "Welcome, new player, what is your name?";
+		return "\033[3mWelcome, new player, what is your name?\033[0m";
 	}
 	
 	/**
@@ -36,7 +36,7 @@ public class SetUpOfGame
 	 */
 	public String checkName(String name)
 	{
-		return "You're name is " + name + "? What an interesting name.";
+		return "\033[3mYou're name is " + name + "? What an interesting name.";
 	}
 	
 	/**
@@ -45,7 +45,7 @@ public class SetUpOfGame
 	 */
 	public static String askQuestion()
 	{
-		return "Do you know what your job is? (yes or no)";
+		return "\033[3mDo you know what your job is? (yes or no)\033[0m";
 	}
 	
 	/**
@@ -55,18 +55,24 @@ public class SetUpOfGame
 	 * @param answer
 	 * @return "You don't! How did you even get hired? You're a detective and ";   
 	 * "Wow. You're better than the last guy we hired... Anyway, "
+	 * @throws Exception 
 	 */
-	public String answerQuestion(String answer)
+	public static String answerQuestion(String answer) throws Exception
 	{
-		if (answer.equalsIgnoreCase("no"))
+		if (answer.indexOf("yes") != -1 && answer.indexOf("no") != -1)
 		{
-			return "You don't! How did you even get hired? Well, you're a detective and ";
+			System.out.print("\n\033[3mbro really... you know what... \n");
+			return forTheJokers();
 		}
-		else if (answer.equalsIgnoreCase("yes"))
+		else if (answer.indexOf("no") != -1)
 		{
-			return "Wow. You're better than the last guy we hired... Anyway, ";
+			return "\n\033[3mYou don't! How did you even get hired? Well, you're a detective and ";
 		}
-		return "oh you're tryna be funny?\n" + forTheJokers();
+		else if (answer.indexOf("yes") != -1)
+		{
+			return "\n\033[3mWow. You're better than the last guy we hired... Anyway, ";
+		}
+		return "\033[3moh you're tryna be funny?\n" + forTheJokers();
 	}
 	
 	/**
@@ -76,16 +82,18 @@ public class SetUpOfGame
 	 */
 	public static String giveExposition()
 	{
-		return "you have a new case assignment. " +
+		return "\033[3myou have a new case assignment. " +
 				"\nA murderer is running rampant and you have to bring them in.";
 	}
 	
 	/**
 	 * This method is for the people that think they're funny and input something that I don't like
 	 * @return a not so nice string
+	 * @throws Exception 
 	 */
-	private static String forTheJokers()
+	private static String forTheJokers () throws Exception
 	{
-		return "The game is over... \nthe murderer killed you on your first day. you rot in hell.";
+		System.out.println("\033[3mthe game is over... \nthe murderer killed you on your first day. you rot in hell.");
+		throw new Exception("don't do that.");
 	}
 }
