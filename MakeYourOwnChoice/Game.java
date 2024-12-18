88e1b66d6c4861242e6b6aa378446bcb5df7fbec
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2024-12-17 20:29:32 -1000

    Update Game.java

--- MakeYourOwnChoice/Game.java
+++ MakeYourOwnChoice/Game.java
@@ -1,16 +1,26 @@
 
 /**
  * This is where the game is played.
+ * THIS IS NOT A COMPLETE GAME
  * @author Keola Teson :)
  */
 public class Game
 {
-			
 	public static void main(String[] args) throws Exception
 	{	
-		//calls readSessionFile method
-		SessionSaver.readSessionFile();
+		Dialogue.setUpGameFiles();
+		
+		//introduces the game to the player
+		Dialogue.setUpGame();
+		
+		//Writes into file "scene1" (currently being unused)
+		Dialogue.sessionSaver.writeIntoFile("scene1");
+		//start of the game
+		Dialogue.makeFirstScene();
+		
 		//closes scanner
 		Dialogue.myScanner.close();
+		
+		Dialogue.deleteFiles();
 	}
 }
