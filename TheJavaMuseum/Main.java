03e1a89257423325b60799c26438e0f632cab25a
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2024-12-20 19:51:17 -1000

    Create Main.java

---
+++ TheJavaMuseum/Main.java
@@ -0,0 +1,18 @@
+
+/**
+ * This is where the game is played. Methods in showcases class are called here
+ * @author Keola Teson
+ */
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		
+		Showcases.setUpGame();
+		
+		Showcases.showcaseUnit1();
+
+	}
+
+}
