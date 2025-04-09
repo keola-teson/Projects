2eb8e3e835fa153f24cb4666ad01ae802c842142
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2025-04-09 11:02:30 -1000

    Update MazeMain.java

--- Maze/MazeMain.java
+++ Maze/MazeMain.java
@@ -3,6 +3,8 @@ public class MazeMain
 {
 	public static void main(String[] args)
 	{
+		Scanner scanner = new Scanner(System.in);
+		
 		Player player = new Player(0, 0, 0);
 		Maze maze = new Maze(new Tile[5][10]);
 		maze.fillMaze(player);
@@ -10,12 +12,14 @@ public class MazeMain
 		while (player.getPosY() != 4 || player.getPosX() != 9)
 		{
 			maze.printMaze();
-			player.checkDirection(maze, new Scanner(System.in));
+			player.checkDirection(maze, scanner);
 		}
 		maze.getMaze()[0][0] = new Tile(false, false, player);
 		
 		maze.printMaze();
 		System.out.println("YOU WIN!");
 		System.out.println("Failure Count: " + player.getCollidedCount());
+		
+		scanner.close();
 	}
 }
