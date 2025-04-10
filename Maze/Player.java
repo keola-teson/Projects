9746b3841d461f3e2f7f67325714b98fa8d1f1af
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2025-04-10 08:20:33 -1000

    Update Player.java

--- Maze/Player.java
+++ Maze/Player.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
-
+/**
+ * PLAYER
+ * where the location and move logic is stores
+ */
 public class Player
 {
 	/* CLASS ATTRIBUTES */
@@ -72,7 +75,7 @@ public class Player
 	}
 	
 	/**
-	 * CHECK
+	 * MOVE
 	 * moves the player in the direction they want to go
 	 * @param maze
 	 * @return true to continue the loop
@@ -82,105 +85,145 @@ public class Player
 		switch (chooseDirection(scanner)) // calls the chooseDirection() method to get the player direction
 		{
 			case "up":
-				if (getPosY() - 1 > -1) // checked for error protection
-				{
-					if (maze.getMaze()[getPosY() - 1][getPosX()].isWall()) // checks if the tile above the player is a wall
-					{
-						System.out.println("You hit a wall.");
-						
-						maze.getMaze()[getPosY() - 1][getPosX()].setRevealed(true); // changes the value of isRevealed for the tile to true
-						// allows it to be printed
-						
-						setCollidedCount(getCollidedCount() + 1); // increments collided count
-					}
-					else
-					{
-						maze.getMaze()[getPosY()][getPosX()].setPlayer(null); // gets rid of the player on the previous tile
-						maze.getMaze()[getPosY() - 1][getPosX()].setPlayer(this); // moves the player by moving the instance of this 
-																				  // class to the tile above
-						
-						maze.getMaze()[getPosY()][getPosX()].setRevealed(true); // reveals path
-						
-						setPosY(getPosY() - 1); // decrements posY
-					}
-				}
+				moveUp(maze);
 				break;
 			
 			case "down":
-				if (getPosY() + 1 < maze.getMaze()[getPosY()].length) // checked for error protection
-				{
-					if (maze.getMaze()[getPosY() + 1][getPosX()].isWall()) // checks if the tile below the player is a wall
-					{
-						System.out.println("You hit a wall.");
-						
-						maze.getMaze()[getPosY() + 1][getPosX()].setRevealed(true); // changes the value of isRevealed for the tile to true
-																					// allows it to be printed
-						
-						setCollidedCount(getCollidedCount() + 1); // increments collided count
-					}
-					else
-					{
-						maze.getMaze()[getPosY()][getPosX()].setPlayer(null); // gets rid of the player on the previous tile
-						maze.getMaze()[getPosY() + 1][getPosX()].setPlayer(this); // moves the player by moving the instance of this 
-																				  // class to the tile below
-						
-						maze.getMaze()[getPosY()][getPosX()].setRevealed(true); // reveals path
-						
-						setPosY(getPosY() + 1); // increments posY
-					}
-				}
+				moveDown(maze);
 				break;
 				
 			case "left":
-				if (getPosX() - 1 > -1) // checked for error protection
-				{
-					if (maze.getMaze()[getPosY()][getPosX() - 1].isWall()) // checks if the tile left of the player is a wall
-					{
-						System.out.println("You hit a wall.");
-						
-						maze.getMaze()[getPosY()][getPosX() - 1].setRevealed(true); // changes the value of isRevealed for the tile to true
-																					// allows it to be printed
-						
-						setCollidedCount(getCollidedCount() + 1); // increments collided count
-					}
-					else
-					{
-						maze.getMaze()[getPosY()][getPosX()].setPlayer(null); // gets rid of the player on the previous tile
-						maze.getMaze()[getPosY()][getPosX() - 1].setPlayer(this); // moves the player by moving the instance of this 
-																				  // class to the tile to the right
-						
-						maze.getMaze()[getPosY()][getPosX()].setRevealed(true); // reveals path
-						
-						setPosX(getPosX() - 1); // increments collided count
-					}
-				}
+				moveLeft(maze);
 				break;
 			
 			case "right":
-				if (getPosX() + 1 < maze.getMaze()[getPosY()].length) // checked for error protection
-				{
-					
-					if (maze.getMaze()[getPosY()][getPosX() + 1].isWall()) // checks if the tile right of the player is a wall
-					{
-						System.out.println("You hit a wall.");
-						
-						maze.getMaze()[getPosY()][getPosX() + 1].setRevealed(true); // changes the value of isRevealed for the tile to true
-																					// allows it to be printed
-						
-						setCollidedCount(getCollidedCount() + 1); // increments collided count
-					}
-					else
-					{
-						maze.getMaze()[getPosY()][getPosX()].setPlayer(null); // gets rid of the player on the previous tile
-						maze.getMaze()[getPosY()][getPosX() + 1].setPlayer(this); // moves the player by moving the instance of this 
-																				  // class to the tile to the right
-						
-						maze.getMaze()[getPosY()][getPosX()].setRevealed(true); // reveals path
-						
-						setPosX(getPosX() + 1); // increments collided count
-					}
-				}
+				moveRight(maze);
 				break;
+		}
+	}
+
+	/**
+	 * MOVE
+	 * moves the player right
+	 * @param maze
+	 */
+	public void moveRight(Maze maze)
+	{
+		if (getPosX() + 1 < maze.getMaze()[getPosY()].length) // checked for error protection
+		{
+			
+			if (maze.getMaze()[getPosY()][getPosX() + 1].isWall()) // checks if the tile right of the player is a wall
+			{
+				System.out.println("You hit a wall.");
+				
+				maze.getMaze()[getPosY()][getPosX() + 1].setRevealed(true); // changes the value of isRevealed for the tile to true
+																			// allows it to be printed
+				
+				setCollidedCount(getCollidedCount() + 1); // increments collided count
+			}
+			else
+			{
+				maze.getMaze()[getPosY()][getPosX()].setPlayer(null); // gets rid of the player on the previous tile
+				maze.getMaze()[getPosY()][getPosX() + 1].setPlayer(this); // moves the player by moving the instance of this 
+																		  // class to the tile to the right
+				
+				maze.getMaze()[getPosY()][getPosX()].setRevealed(true); // reveals path
+				
+				setPosX(getPosX() + 1); // increments collided count
+			}
+		}
+	}
+
+	/**
+	 * MOVE
+	 * moves the player left
+	 * @param maze
+	 */
+	public void moveLeft(Maze maze)
+	{
+		if (getPosX() - 1 > -1) // checked for error protection
+		{
+			if (maze.getMaze()[getPosY()][getPosX() - 1].isWall()) // checks if the tile left of the player is a wall
+			{
+				System.out.println("You hit a wall.");
+				
+				maze.getMaze()[getPosY()][getPosX() - 1].setRevealed(true); // changes the value of isRevealed for the tile to true
+																			// allows it to be printed
+				
+				setCollidedCount(getCollidedCount() + 1); // increments collided count
+			}
+			else
+			{
+				maze.getMaze()[getPosY()][getPosX()].setPlayer(null); // gets rid of the player on the previous tile
+				maze.getMaze()[getPosY()][getPosX() - 1].setPlayer(this); // moves the player by moving the instance of this 
+																		  // class to the tile to the right
+				
+				maze.getMaze()[getPosY()][getPosX()].setRevealed(true); // reveals path
+				
+				setPosX(getPosX() - 1); // increments collided count
+			}
+		}
+	}
+
+	/**
+	 * MOVE
+	 * moves the player down
+	 * @param maze
+	 */
+	public void moveDown(Maze maze)
+	{
+		if (getPosY() + 1 < maze.getMaze()[getPosY()].length) // checked for error protection
+		{
+			if (maze.getMaze()[getPosY() + 1][getPosX()].isWall()) // checks if the tile below the player is a wall
+			{
+				System.out.println("You hit a wall.");
+				
+				maze.getMaze()[getPosY() + 1][getPosX()].setRevealed(true); // changes the value of isRevealed for the tile to true
+																			// allows it to be printed
+				
+				setCollidedCount(getCollidedCount() + 1); // increments collided count
+			}
+			else
+			{
+				maze.getMaze()[getPosY()][getPosX()].setPlayer(null); // gets rid of the player on the previous tile
+				maze.getMaze()[getPosY() + 1][getPosX()].setPlayer(this); // moves the player by moving the instance of this 
+																		  // class to the tile below
+				
+				maze.getMaze()[getPosY()][getPosX()].setRevealed(true); // reveals path
+				
+				setPosY(getPosY() + 1); // increments posY
+			}
+		}
+	}
+
+	/**
+	 * MOVE
+	 * moves the player up
+	 * @param maze
+	 */
+	public void moveUp(Maze maze)
+	{
+		if (getPosY() - 1 > -1) // checked for error protection
+		{
+			if (maze.getMaze()[getPosY() - 1][getPosX()].isWall()) // checks if the tile above the player is a wall
+			{
+				System.out.println("You hit a wall.");
+				
+				maze.getMaze()[getPosY() - 1][getPosX()].setRevealed(true); // changes the value of isRevealed for the tile to true
+				// allows it to be printed
+				
+				setCollidedCount(getCollidedCount() + 1); // increments collided count
+			}
+			else
+			{
+				maze.getMaze()[getPosY()][getPosX()].setPlayer(null); // gets rid of the player on the previous tile
+				maze.getMaze()[getPosY() - 1][getPosX()].setPlayer(this); // moves the player by moving the instance of this 
+																		  // class to the tile above
+				
+				maze.getMaze()[getPosY()][getPosX()].setRevealed(true); // reveals path
+				
+				setPosY(getPosY() - 1); // decrements posY
+			}
 		}
 	}
 	
@@ -188,7 +231,7 @@ public class Player
 	 * allows player to choose direction
 	 * @return up, down, left, or right
 	 */
-	private String chooseDirection(Scanner scanner)
+	public String chooseDirection(Scanner scanner)
 	{
 		String direction = scanner.nextLine().toLowerCase(); // stores player input
 		
