cbcdb6e2db510d7450f5eac9b2d46149b4d5e1c5
Keola Teson <84607397+keola-teson@users.noreply.github.com>
2025-04-10 08:19:25 -1000

    Update MazeMain.java

--- Maze/MazeMain.java
+++ Maze/MazeMain.java
@@ -1,25 +1,50 @@
+import java.util.Scanner;
 
+import maze.Maze;
+import player.Flashlight;
+import player.Player;
+
+/**
+ * Where the game is played
+ */
 public class MazeMain
 {
 	public static void main(String[] args)
 	{
-		Scanner scanner = new Scanner(System.in);
+		playGame();
+	}
+	
+	static void playGame()
+	{
+		Scanner scanner = new Scanner(System.in); // input
 		
-		Player player = new Player(0, 0, 0);
-		Maze maze = new Maze(new Tile[5][10]);
-		maze.fillMaze(player);
+		Player player = chooseGamemode(scanner); // player object
+		Maze maze = new Maze(new char[][] {
+			{'8', '|', '|', ' ', ' ', ' ', ' ', ' ', ' ', '|'},
+			{' ', '|', ' ', ' ', ' ', '|', '|', ' ', '|', '|'},
+			{' ', ' ', ' ', '|', ' ', ' ', ' ', '|', ' ', '|'},
+			{' ', '|', '|', ' ', ' ', '|', ' ', '|', ' ', '|'},
+			{' ', '|', ' ', ' ', '|', ' ', ' ', ' ', ' ', ' '}}, player); // maze object with Tile map passed into it
 		
-		while (player.getPosY() != 4 || player.getPosX() != 9)
+		while (player.getPosY() != 4 || player.getPosX() != 9) // loops until player hit win
 		{
-			maze.printMaze();
 			player.checkDirection(maze, scanner);
+			maze.printMaze();
 		}
-		maze.getMaze()[0][0] = new Tile(false, false, player);
-		
-		maze.printMaze();
-		System.out.println("YOU WIN!");
-		System.out.println("Failure Count: " + player.getCollidedCount());
+		System.out.println("YOU WIN!\nFailure Count: " + player.getCollidedCount());
 		
 		scanner.close();
+	}
+	
+	static Player chooseGamemode(Scanner scanner)
+	{
+		System.out.println("Choose Gamemode (Default | Flashlight)");
+		String input = scanner.nextLine();
+		
+		if (input.equalsIgnoreCase("flashlight"))
+		{
+			return new Flashlight(0, 0, 0);
+		}
+		return new Player(0, 0, 0);
 	}
 }
